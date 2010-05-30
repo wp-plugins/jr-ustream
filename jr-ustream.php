7a4dda37928f3e34633d9e1554cec490ae43b69a
jruston <jruston@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-30 15:55:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/jr-ustream/trunk@247299 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jr-ustream.php
@@ -0,0 +1,350 @@
+<?php
+/*
+Plugin Name: JR Ustream
+Plugin URI: http://www.jakeruston.co.uk/2010/05/wordpress-plugin-jr-ustream/
+Description: Allows you to Ustream various objects into your posts - Such as YouTube videos, images and music.
+Version: 1.0.0
+Author: Jake Ruston
+Author URI: http://www.jakeruston.co.uk
+*/
+
+/*  Copyright 2010 Jake Ruston - the.escapist22@gmail.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+$pluginname="Ustream";
+
+// Hook for adding admin menus
+add_action('admin_menu', 'jr_Ustream_add_pages');
+
+// action function for above hook
+function jr_Ustream_add_pages() {
+    add_options_page('JR Ustream', 'JR Ustream', 'administrator', 'jr_Ustream', 'jr_Ustream_options_page');
+}
+
+if (!function_exists("_iscurlinstalled")) {
+function _iscurlinstalled() {
+if (in_array ('curl', get_loaded_extensions())) {
+return true;
+} else {
+return false;
+}
+}
+}
+
+if(_iscurlinstalled()==false) {
+if (!function_exists("curl_init")) {
+function curl_init() {
+}
+}
+
+if (!function_exists("curl_setopt")) {
+function curl_init($i, $j, $k) {
+}
+}
+
+if (!function_exists("setupch")) {
+function setupch() {
+}
+}
+
+if (!function_exists("curl_get_contents")) {
+function curl_get_contents($i) {
+}
+}
+}
+
+if (_iscurlinstalled()) {
+if (!defined("ch"))
+{
+function setupch()
+{
+$ch = curl_init();
+$c = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+return($ch);
+}
+define("ch", setupch());
+}
+
+if (!function_exists("curl_get_contents")) {
+function curl_get_contents($url)
+{
+$c = curl_setopt(ch, CURLOPT_URL, $url);
+return(curl_exec(ch));
+}
+}
+}
+
+register_activation_hook(__FILE__,'Ustream_choice');
+
+function Ustream_choice () {
+if (get_option("jr_Ustream_links_choice")=="") {
+
+if (_iscurlinstalled()) {
+$content = curl_get_contents("http://www.jakeruston.co.uk/pluginslink4.php");
+} else {
+$content = "Powered by <a href='http://arcade.xeromi.com'>Free Online Games</a> and <a href='http://directory.xeromi.com'>General Web Directory</a>.";
+}
+
+if ($content!="") {
+$content=utf8_encode($content);
+update_option("jr_Ustream_links_choice", $content);
+}
+}
+
+if (get_option("jr_Ustream_link_personal")=="") {
+$rand=rand(1,2);
+
+switch ($rand) {
+case 1:
+$anch="Jake Ruston's <a href='http://www.jakeruston.co.uk'>Wordpress Plugins</a>";
+break;
+case 2:
+$anch="<a href='http://www.xeromi.net'>Cheap Web Hosting</a>";
+break;
+}
+
+update_option("jr_Ustream_link_personal", $anch);
+}
+}
+
+// jr_Ustream_options_page() displays the page content for the Test Options submenu
+function jr_Ustream_options_page() {
+
+    // variables for the field and option names 
+    $opt_name_5 = 'mt_Ustream_plugin_support';
+    $hidden_field_name = 'mt_Ustream_submit_hidden';
+    $data_field_name_5 = 'mt_Ustream_plugin_support';
+
+    // Read in existing option value from database
+    $opt_val_5 = get_option($opt_name_5);
+    
+if (!$_POST['feedback']=='') {
+$my_email1="the.escapist22@gmail.com";
+$plugin_name="JR Ustream";
+$blog_url_feedback=get_bloginfo('url');
+$user_email=$_POST['email'];
+$user_email=stripslashes($user_email);
+$subject=$_POST['subject'];
+$subject=stripslashes($subject);
+$name=$_POST['name'];
+$name=stripslashes($name);
+$response=$_POST['response'];
+$response=stripslashes($response);
+$category=$_POST['category'];
+$category=stripslashes($category);
+if ($response=="Yes") {
+$response="REQUIRED: ";
+}
+$feedback_feedback=$_POST['feedback'];
+$feedback_feedback=stripslashes($feedback_feedback);
+if ($user_email=="") {
+$headers1 = "From: feedback@jakeruston.co.uk";
+} else {
+$headers1 = "From: $user_email";
+}
+$emailsubject1=$response.$plugin_name." - ".$category." - ".$subject;
+$emailmessage1="Blog: $blog_url_feedback\n\nUser Name: $name\n\nUser E-Mail: $user_email\n\nMessage: $feedback_feedback";
+mail($my_email1,$emailsubject1,$emailmessage1,$headers1);
+?>
+
+<div class="updated"><p><strong><?php _e('Feedback Sent!', 'mt_trans_domain' ); ?></strong></p></div>
+
+<?php
+}
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+        $opt_val_5 = $_POST[$data_field_name_5];
+
+        // Save the posted value in the database
+        update_option( $opt_name_5, $opt_val_5 );
+
+        // Put an options updated message on the screen
+
+?>
+<div class="updated"><p><strong><?php _e('Options saved.', 'mt_trans_domain' ); ?></strong></p></div>
+<?php
+
+    }
+
+    // Now display the options editing screen
+
+    echo '<div class="wrap">';
+
+    // header
+
+    echo "<h2>" . __( 'JR Ustream Plugin Options', 'mt_trans_domain' ) . "</h2>";
+
+$blog_url_feedback=get_bloginfo('url');
+	$donated=curl_get_contents("http://www.jakeruston.co.uk/p-donation/index.php?url=".$blog_url_feedback);
+	if ($donated=="1") {
+	?>
+		<div class="updated"><p><strong><?php _e('Thank you for donating!', 'mt_trans_domain' ); ?></strong></p></div>
+	<?php
+	} else {
+	if ($_POST['mtdonationjr']!="") {
+	update_option("mtdonationjr", "444");
+	}
+	
+	if (get_option("mtdonationjr")=="") {
+	?>
+	<div class="updated"><p><strong><?php _e('Please consider donating to help support the development of my plugins!', 'mt_trans_domain' ); ?></strong><br /><br /><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="ULRRFEPGZ6PSJ">
+<input type="image" src="https://www.paypal.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
+<img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form></p><br /><form action="" method="post"><input type="hidden" name="mtdonationjr" value="444" /><input type="submit" value="Don't Show This Again" /></form></div>
+<?php
+}
+}
+
+    // options form
+    
+    $change3 = get_option("mt_Ustream_plugin_support");
+
+if ($change3=="Yes" || $change3=="") {
+$change3="checked";
+$change31="";
+} else {
+$change3="";
+$change31="checked";
+}
+    ?>
+	<iframe src="http://www.jakeruston.co.uk/plugins/index.php" width="100%" height="20%">iframe support is required to see this.</iframe>
+<form name="form1" method="post" action="">
+<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+
+<p><strong>How to Use:</strong></p>
+
+<p>Simply enter a Ustream URL into one of your posts or pages and Ustream will automatically be embeded.</p>
+
+<p><?php _e("Show Plugin Support?", 'mt_trans_domain' ); ?> 
+<input type="radio" name="<?php echo $data_field_name_5; ?>" value="Yes" <?php echo $change3; ?>>Yes
+<input type="radio" name="<?php echo $data_field_name_5; ?>" value="No" <?php echo $change31; ?> id="Please do not disable plugin support - This is the only thing I get from creating this free plugin!" onClick="alert(id)">No
+</p>
+
+<p class="submit">
+<input type="submit" name="Submit" value="<?php _e('Update Options', 'mt_trans_domain' ) ?>" />
+</p><hr />
+
+</form>
+<script type="text/javascript">
+function validate_required(field,alerttxt)
+{
+with (field)
+  {
+  if (value==null||value=="")
+    {
+    alert(alerttxt);return false;
+    }
+  else
+    {
+    return true;
+    }
+  }
+}
+
+function validateEmail(ctrl){
+
+var strMail = ctrl.value
+        var regMail =  /^\w+([-.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+        if (regMail.test(strMail))
+        {
+            return true;
+        }
+        else
+        {
+
+            return false;
+
+        }
+					
+	}
+
+function validate_form(thisform)
+{
+with (thisform)
+  {
+  if (validate_required(subject,"Subject must be filled out!")==false)
+  {email.focus();return false;}
+  if (validate_required(email,"E-Mail must be filled out!")==false)
+  {email.focus();return false;}
+  if (validate_required(feedback,"Feedback must be filled out!")==false)
+  {email.focus();return false;}
+  if (validateEmail(email)==false)
+  {
+  alert("E-Mail Address not valid!");
+  email.focus();
+  return false;
+  }
+ }
+}
+</script>
+<h3>Submit Feedback about my Plugin!</h3>
+<p><b>Note: Only send feedback in english, I cannot understand other languages!</b><br /><b>Please do not send spam messages!</b></p>
+<form name="form2" method="post" action="" onsubmit="return validate_form(this)">
+<p><?php _e("Your Name:", 'mt_trans_domain' ); ?> 
+<input type="text" name="name" /></p>
+<p><?php _e("E-Mail Address (Required):", 'mt_trans_domain' ); ?> 
+<input type="text" name="email" /></p>
+<p><?php _e("Message Category:", 'mt_trans_domain'); ?>
+<select name="category">
+<option value="General">General</option>
+<option value="Feedback">Feedback</option>
+<option value="Bug Report">Bug Report</option>
+<option value="Feature Request">Feature Request</option>
+<option value="Other">Other</option>
+</select>
+<p><?php _e("Message Subject (Required):", 'mt_trans_domain' ); ?>
+<input type="text" name="subject" /></p>
+<input type="checkbox" name="response" value="Yes" /> I want e-mailing back about this feedback</p>
+<p><?php _e("Message Comment (Required):", 'mt_trans_domain' ); ?> 
+<textarea name="feedback"></textarea>
+</p>
+<p class="submit">
+<input type="submit" name="Send" value="<?php _e('Send', 'mt_trans_domain' ); ?>" />
+</p><hr /></form>
+</div>
+<?php } ?>
+<?php
+if (get_option("jr_Ustream_links_choice")=="") {
+Ustream_choice();
+}
+
+if (function_exists('wp_oembed_add_provider')) {
+add_action('plugins_loaded', 'jrustream_enable');
+}
+
+function jrustream_enable() {
+	wp_oembed_add_provider('#http://(www\.)?ustream.tv/*#i', 'http://www.ustream.tv/oembed', true);
+}
+
+$supportplugin=get_option("mt_Ustream_plugin_support");
+if ($supportplugin=="" || $supportplugin=="Yes") {
+add_action('wp_footer', 'Ustream_footer_plugin_support');
+}
+
+function Ustream_footer_plugin_support() {
+$linkper=utf8_decode(get_option('jr_Ustream_link_personal'));
+  $pshow = "<p style='font-size:x-small'>Ustream Plugin created by ".$linkper." - ".get_option('jr_Ustream_links_choice')."</p>";
+  echo $pshow;
+}
+
+?>
